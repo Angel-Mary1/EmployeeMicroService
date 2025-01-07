2baa50471fa016347f456d738d58ef9a7b43b3a9
amary-bbdn <angelm@anthology.com>
2025-01-08 00:14:07 +0530

    oneToMany association

--- src/main/java/Practice/springboot/employe/controller/OrderController.java
+++ src/main/java/Practice/springboot/employe/controller/OrderController.java
@@ -6,7 +6,6 @@ import Practice.springboot.employe.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
